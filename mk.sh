11bbb11c5ebdad29c4089c80d24c0fbf588114fa
Charles Shiflett <cshiflett@es.net>
2024-11-15 11:44:09 -0800

    ..

--- mk.sh
+++ mk.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # run to build escp
-if [ ! -d "build" ]; then
-  mkdir build && cd build && cmake ../libdtn && cd .. || (echo cargo:error="CMAKE BUILD FAILED" && cd .. && rmdir build)
+if [ ! -d "target/dtn" ]; then
+  mkdir -p target/dtn && cd target/dtn && cmake ../../libdtn && cd ../.. || (echo cargo:error="CMAKE BUILD FAILED" && cd ../.. && rmdir target/dtn )
 fi
 
 if [ -z $1 ]; then
-  cd build && make -j 24 && cd .. && cargo build || echo cargo:error="MAKE FAILED"
+  cd target/dtn && make -j 24 && cd ../.. && cargo build || echo cargo:error="MAKE FAILED"
 else 
-  cd build && make -j 24 && cd .. || echo cargo:error="MAKE FAILED"
+  cd target/dtn && make -j 24 && cd ../.. || echo cargo:error="MAKE FAILED"
 fi
--- src/build.rs
+++ src/build.rs
@@ -1,14 +1,14 @@
 use std::process::Command;
 
 fn main() -> shadow_rs::SdResult<()>  {
-    println!("cargo:rustc-link-arg-bins=build/libdtn.a");
-    println!("cargo:rustc-link-arg-bins=build/isal/lib/libisal_crypto.a");
-    println!("cargo:rustc-link-arg-bins=build/libnuma/lib/libnuma.a");
-    println!("cargo:rustc-link-arg-bins=build/zstd/lib/libzstd.a");
+    println!("cargo:rustc-link-arg-bins=target/dtn/libdtn.a");
+    println!("cargo:rustc-link-arg-bins=target/dtn/isal/lib/libisal_crypto.a");
+    println!("cargo:rustc-link-arg-bins=target/dtn/libnuma/lib/libnuma.a");
+    println!("cargo:rustc-link-arg-bins=target/dtn/zstd/lib/libzstd.a");
     println!("cargo:rustc-link-arg-bins=-lrt");
     println!("cargo:rustc-link-arg-bins=-lc");
     println!("cargo:rustc-link-arg-bins=-no-pie");
     Command::new("sh").args(["./mk.sh", "argument"]).status().unwrap();
-    println!("cargo:rerun-if-changed=build/libdtn.a");
+    println!("cargo:rerun-if-changed=target/dtn/libdtn.a");
     shadow_rs::new()
 }
