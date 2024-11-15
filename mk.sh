bcee041f4784b8e4f2d1f61ece3959f075afa03f
Charles Shiflett <cshiflett@es.net>
2024-11-15 11:32:39 -0800

    ..

--- mk.sh
+++ mk.sh
@@ -5,4 +5,8 @@ if [ ! -d "build" ]; then
   mkdir build && cd build && cmake ../libdtn && cd .. || (echo cargo:error="CMAKE BUILD FAILED" && cd .. && rmdir build)
 fi
 
-cd build && make -j 24 && cd .. && cargo build || echo cargo:error="MAKE FAILED"
+if [ -z $1 ]; then
+  cd build && make -j 24 && cd .. && cargo build || echo cargo:error="MAKE FAILED"
+else 
+  cd build && make -j 24 && cd .. || echo cargo:error="MAKE FAILED"
+fi
