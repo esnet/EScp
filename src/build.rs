6ffa5d50adc3dc43308dc57e7c5dfcb2c34cd4e4
Your Name <you@example.com>
2025-04-08 11:00:21 -0700

    Backport shadow_rs fixes

--- src/build.rs
+++ src/build.rs
@@ -1,14 +1,26 @@
-// use std::process::Command;
+use std::process::Command;
+use shadow_rs::BuildPattern;
+use shadow_rs::ShadowBuilder;
 
-fn main() -> shadow_rs::SdResult<()>  {
-    println!("cargo:rustc-link-arg-bins=build/libdtn.a");
-    println!("cargo:rustc-link-arg-bins=build/isal/lib/libisal_crypto.a");
-    println!("cargo:rustc-link-arg-bins=build/libnuma/lib/libnuma.a");
-    println!("cargo:rustc-link-arg-bins=build/zstd/lib/libzstd.a");
+fn main() {
+    println!("cargo:rustc-link-arg-bins=target/dtn/libdtn.a");
+    println!("cargo:rustc-link-arg-bins=target/dtn/isal/lib/libisal_crypto.a");
+    println!("cargo:rustc-link-arg-bins=target/dtn/libnuma/lib/libnuma.a");
+    println!("cargo:rustc-link-arg-bins=target/dtn/zstd/lib/libzstd.a");
     println!("cargo:rustc-link-arg-bins=-lrt");
     println!("cargo:rustc-link-arg-bins=-lc");
     println!("cargo:rustc-link-arg-bins=-no-pie");
-    // Command::new("sh").args(["mk.sh", "argument"]).status().unwrap();
-    println!("cargo:rerun-if-changed=build/libdtn.a");
-    shadow_rs::new()
+    println!("cargo:rustc-link-arg-tests=target/dtn/libdtn.a");
+    println!("cargo:rustc-link-arg-tests=target/dtn/isal/lib/libisal_crypto.a");
+    println!("cargo:rustc-link-arg-tests=target/dtn/libnuma/lib/libnuma.a");
+    println!("cargo:rustc-link-arg-tests=target/dtn/zstd/lib/libzstd.a");
+    println!("cargo:rustc-link-arg-tests=-lrt");
+    println!("cargo:rustc-link-arg-tests=-lc");
+    println!("cargo:rustc-link-arg-tests=-no-pie");
+     Command::new("sh").args(["./mk.sh", "argument"]).status().unwrap();
+    println!("cargo:rerun-if-changed=target/dtn/libdtn.a");
+    ShadowBuilder::builder()
+        .build_pattern(BuildPattern::RealTime)
+        .build().unwrap();
 }
+
