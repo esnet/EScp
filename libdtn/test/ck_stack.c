c499f524d7376b4f21ac44556b64cc2e84e631b2
Charles Shiflett <cshiflett@es.net>
2026-02-04 17:05:43 -0800

    Update engine_dummy

--- libdtn/test/ck_stack.c
+++ libdtn/test/ck_stack.c
@@ -4,59 +4,58 @@
 
 #define MAX_FDS 1024
 
-typedef struct fd_node {
+typedef struct item {
     int fd;
     ck_stack_entry_t next;
-} fd_node_t;
+} item_t;
 
-static ck_stack_t free_fds;
-CK_STACK_CONTAINER(fd_node_t, next, fd_node_container)
+static ck_stack_t stack = CK_STACK_INITIALIZER;
+static item_t items[MAX_FDS];
 
-void fd_init() {
-    ck_stack_init(&free_fds);
+CK_STACK_CONTAINER(item_t, next, item_container)
+
+void stack_open() {
+    ck_stack_init(&stack);
     
     // Pre-populate with all FDs in reverse order
     // (so popping gives lowest first)
     for (int i = MAX_FDS - 1; i >= 0; i--) {
-        fd_node_t *node = malloc(sizeof(fd_node_t));
-        node->fd = i;
-        ck_stack_push_upmc(&free_fds, &node->next);
+        items[i].fd = i;
+        ck_stack_push_mpmc(&stack, &items[i].next);
     }
 }
 
-int fd_open(void *data, size_t size) {
-    ck_stack_entry_t *entry = ck_stack_pop_upmc(&free_fds);
+int stack_pop(void *data, size_t size) {
+    ck_stack_entry_t *entry = ck_stack_pop_mpmc(&stack);
     if (!entry) {
         return -1;  // No free FDs
     }
     
-    fd_node_t *node = fd_node_container(entry);
-    int fd = node->fd;
-    free(node);
-    
-    return fd;
+    item_t *item = item_container(entry);
+    return item->fd;
 }
 
-void fd_close(int fd) {
+void stack_push(int fd) {
+    if (fd < 0 || fd >= MAX_FDS) {
+        return;
+    }
     // Return to free list
-    fd_node_t *node = malloc(sizeof(fd_node_t));
-    node->fd = fd;
-    ck_stack_push_upmc(&free_fds, &node->next);
+    ck_stack_push_mpmc(&stack, &items[fd].next);
 }
 
 int main() {
-    fd_init();
+    stack_open();
 
-    int fd1 = fd_open(NULL, 0);
-    int fd2 = fd_open(NULL, 0);
-    int fd3 = fd_open(NULL, 0);
+    int fd1 = stack_pop(NULL, 0);
+    int fd2 = stack_pop(NULL, 0);
+    int fd3 = stack_pop(NULL, 0);
 
     printf("Opened FDs: %d, %d, %d\n", fd1, fd2, fd3);
 
-    fd_close(fd2);
+    stack_push(fd2);
     printf("Closed FD: %d\n", fd2);
 
-    int fd4 = fd_open(NULL, 0);
+    int fd4 = stack_pop(NULL, 0);
     printf("Opened FD (should be %d): %d\n", fd2, fd4);
 
     if (fd4 == fd2) {
